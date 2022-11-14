3b0ece7b45ba895e6ef1a38dfa704bd881cb4fcc
Fahrian <73996601+fchrgrib2310@users.noreply.github.com>
2022-11-14 09:40:12 +0700

    covariance.py

---
+++ src/covariance.py
@@ -0,0 +1,43 @@
+import subtractAvAndTrain as subs
+
+
+def transpose(matrix):
+    trans = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
+    for i in range(matrix[0]):
+        for j in range(matrix):
+            trans[i][j] = matrix[j][i]
+    return trans
+
+
+def multiplyMtrx(mtrx1,mtrx2):
+    result = [[0 for i in range(len(mtrx1))] for j in range(mtrx2[0])]
+    rang = len(mtrx1[0])
+    for i in range(len(mtrx1)):
+        for j in range(len(mtrx2[0])):
+            for k in range(rang):
+                result[i][j] += mtrx1[i][k]*mtrx2[k][j]
+    return result
+
+
+def covariance(path):
+    A = [[0 for i in range(256)] for j in range(256)]
+    x = subs.subtractAvAndTrain(path)
+    all = len(x)
+    for i in range(all):
+        for j in range(256):
+            for k in range(256):
+                A[j][k] += x[i][j][k]
+    return multiplyMtrx(A, transpose(A))
+
+
+def L(path):
+    A = [[0 for i in range(256)] for j in range(256)]
+    x = subs.subtractAvAndTrain(path)
+    all = len(x)
+    for i in range(all):
+        for j in range(256):
+            for k in range(256):
+                A[j][k] += x[i][j][k]
+    return multiplyMtrx(transpose(A), A)
+
+
