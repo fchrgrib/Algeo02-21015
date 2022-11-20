8e3340e394c4a49461fb70dcb84ea6de09df1045
Fahrian <73996601+fchrgrib2310@users.noreply.github.com>
2022-11-21 04:12:58 +0700

    revisi all code

--- src/backend/compareImage.py
+++ src/backend/compareImage.py
@@ -1,4 +1,7 @@
 import math
+
+import numpy
+
 import tools.subtractAvAndTrain as sub
 import tools.eigenValVec as eigenVec
 import tools.eigenFace as eigFace
@@ -8,23 +11,27 @@ def compareImage(imageNew, imageOld):
     # Mendapatkan selisih antara image trainer dan nilai tengah
     imgNew = sub.subtractAvAndTrain(imageNew)
     imgOld = sub.subtractAvAndTrain(imageOld)
-
-    vNew = [0 for i in range (len(imgNew))]
+    print(len(imgNew))
+    vNew = ([0 for i in range (len(imgNew))])
     vOld = [0 for i in range(len(imgOld))]
     for i in range (len(imgNew)):
-        w,vNew[i] = eigenVec.eigenValVec(np.matrix(imgNew[i]))
+        vNew[i] = eigenVec.eigenValVec(np.matrix(imgNew[i]))[1]
     for i in range(len(imgOld)):
-        w,vOld[i] = eigenVec.eigenValVec(np.matrix(imgOld[i]))
+        vOld[i] = eigenVec.eigenValVec(np.matrix(imgOld[i]))[1]
     # vNew = eigenVec.eigenValVec(imgNew)
     # vOld = eigenVec.eigenValVec(imgOld)
-    
+    # print(vNew)
+    # print(vOld)
     newFace = eigFace.eigenFace(vNew, imageNew)
     oldFace = eigFace.eigenFace(vOld, imageOld)
+    # NnewFace = [[0 for i in range(256)]for j in range(len(newFace[0]))]
+
+    # print(oldFace)
     
     result = 0
-    for i in range (len(newFace)):
-        for j in range (len(oldFace)):
-            result += (abs(newFace[i][j] - oldFace[i][j]))**2
+    # for i in range (len(newFace)):
+    #     for j in range (len(oldFace)):
+    result = (abs(np.subtract(newFace,oldFace)))**2
     result = np.sqrt(result)
     result = np.sum(result)
     return result
--- src/backend/main.py
+++ src/backend/main.py
@@ -6,5 +6,5 @@ print("Hello world")
 # print(comp.compareImage("././test/classes_pins_dataset/coba1","././test/classes_pins_dataset/coba2"))
 # subtract = [[[0 for i in range(256)] for j in range(256)] for k in range(5)]
 # print(len(subtract))
-print(comp.compareImage("././test/classes_pins_dataset/coba1","././test/small_dataset/11"))
+print(100-comp.compareImage("../../test/classes_pins_dataset/coba1","../../test/small_dataset/10")/(256**2*5))
 print("--- %s seconds ---" % (time.time() - start_time))
