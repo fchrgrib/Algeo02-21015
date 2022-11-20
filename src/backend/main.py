b4da1d5a8672b1cd1d4f3e19ba5fc0fab933d919
Fahrian <73996601+fchrgrib2310@users.noreply.github.com>
2022-11-20 22:07:12 +0700

    revisi all code

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,7 +1,7 @@
 import compareImage as comp
 
 print("Hello world")
-print(comp.compareImage("././test/small_dataset/1","././test/small_dataset/1"))
+print(comp.compareImage("../../test/classes_pins_dataset/coba1","../../test/classes_pins_dataset/coba2"))
 # subtract = [[[0 for i in range(256)] for j in range(256)] for k in range(5)]
 # print(len(subtract))
 
