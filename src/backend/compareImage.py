b4f1b0138428a094b4ee696497765dab3df5fec3
Fahrian <73996601+fchrgrib2310@users.noreply.github.com>
2022-11-22 09:53:55 +0700

    revisi all code

--- src/backend/compareImage.py
+++ src/backend/compareImage.py
@@ -10,62 +10,30 @@ import numpy as np
 def compareImage(imageNew, imageOld):
     # Mendapatkan selisih antara image trainer dan nilai tengah
     imgNew = cov.getHimpunanImgV3(imageNew)
-    ImgOld = cov.getHimpunanImgV3(imageOld)
     subOld = sub.subtractAvAndTrain(imageOld)
     rataImgOld = cov.averageImgV7(imageOld)
     immNew = np.subtract(imgNew,rataImgOld)
-    # print(len(immNew))
-    # print(len(immNew[0]))
-
-    # print(subNew)
-    # subNewTrans = np.transpose(subNew)
-    # print(len(subNew))
-    # print(len(subNewTrans))
-    # print(len(np.transpose(cov.substractAllHimpunan(imgNew))))
     subOldTrans =[0 for i in range(5)]
     for i in range (len(subOld)):
         subOldTrans[i] = np.transpose(subOld[i])
     covOld = cov.covarianceGreek(imageOld)
-    # print(covNew)
-    # print(covOld)
-    # eigVecNew = eigenVec.getEigenVectorQR(np.linalg.,eigenVec.getEigenValueQR(imgNew,100))
+    a,v = np.linalg.eig(covOld)
     eigVecOld = [0 for i in range(5)]
     for i in range(5):
         eigVecOld[i] = eigenVec.getEigenVectorQR(covOld[i], eigenVec.getEigenValueQR(covOld[i],1))
     newFace = [0 for i in range(5)]
     for i in range(len(eigVecOld)):
-        newFace[i] = np.matmul(eigVecOld[i],immNew)
-    # eigVecNew = eigenVec.getEigenVectorQR(cov.covarianceManual(imageNew), eigenVec.getEigenValueQR(cov.covarianceManual(imageNew),100))
-    # multEig = np.array([0 for i in range(5)])
-    # multEig = np.dot(eigVecOld,image1[0])
-    # print(multEig)
-    # vNew = ([0 for i in range (len(imgNew))])
-    # vOld = [0 for i in range(len(imgOld))]
-    # for i in range (len(imgNew)):
-    #     vNew[i] = eigenVec.eigenValVecVercit(np.matrix(imgNew[i]))[1]
-    # for i in range(len(imgOld)):
-    #     vOld[i] = eigenVec.eigenValVecVercit(np.matrix(imgOld[i]))[1]
-    # vNew = eigenVec.eigenValVec(imgNew)
-    # vOld = eigenVec.eigenValVec(imgOld)
-    # print(vNew)
-    # print(vOld)
-
-    # newFace = eigenVec.getEigenFace(eigVecNew,imageOld)
-    # oldFace = eigenVec.getEigenFace(eigVecOld,imageOld)
-    # print(eigenVec.getRealEigenVector(imgNew,eigVecNew))
-    # print(eigenVec.getEigenFace(imgNew,eigenVec.getRealEigenVector(imgNew,eigVecNew)))
+        newFace[i] = np.matmul(eigVecOld[i],immNew[0])
     oldFace = [0 for i in range(5)]
     for i in range(5):
         oldFace[i] = eigenVec.getEigenFace(subOldTrans[i], eigenVec.getRealEigenVector(subOldTrans[i], eigVecOld[i]))
-    # NnewFace = [[0 for i in range(256)]for j in range(len(newFace[0]))]
-    # print(oldFace)
-    
-    result = 0
-    # for i in range (len(newFace)):
-    #     for j in range (len(oldFace)):
-    # print(oldFace)
-    result = (abs(np.subtract(newFace,oldFace)))**2
-    result = np.sum(result)
+    anu =0
+    for i in range(5):
+        anu = np.add(anu,newFace[i])
+    arsult = [0 for i in range(5)]
+    for i in range (5):
+        arsult[i] = (abs(np.subtract(anu,oldFace[i])))
+    result = np.sum(arsult)
     result = np.sqrt(result)
     print(result)
     return result
@@ -76,6 +44,7 @@ def compareAllImage(imageNew, AllImage):
     index = 0
     for i in range (10):
         result = compareImage(imageNew, AllImage+"/"+str(i+1))
+        print(i+1)
         if (result < min):
             min = result
             index = i
