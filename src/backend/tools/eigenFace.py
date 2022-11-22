b9a5fb0d0e0a0603eac9f0452612f0c60130883f
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2022-11-22 20:42:04 +0700

    Add: comments on eigenFace

--- src/backend/tools/eigenFace.py
+++ src/backend/tools/eigenFace.py
@@ -1,6 +1,9 @@
 import src.backend.tools.covariance as cov
 import src.backend.tools.subtractAvAndTrain as sub
 import numpy as np
+import src.backend.tools.eigenValVec as EV
+import src.backend.tools.averageImage as AV
+import src.backend.tools.covarianceRemake as CR
 
 
 def eigenFace(vector, path):
@@ -11,3 +14,20 @@ def eigenFace(vector, path):
         eig = np.dot(vector,subtract[i])
     # print(eig)
     return eig
+
+
+''' path = './././test/small_dataset/1'
+S = CR.getHimpunanImgV3(path)
+covar = CR.covarianceManual(path)
+eigVal = EV.getEigenValueQR(covar,30)
+eigVec = EV.getEigenVectorQR(covar, eigVal)
+tesFace = eigenFace(eigVec,path)
+
+print(np.matrix(eigVec))
+print(("=============================="))
+e , v = np.linalg.eig(covar)
+print(v)
+
+print(("=============================="))
+
+print(tesFace) '''
